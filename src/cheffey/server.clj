f9808479cf49a11c1349c02f4b84a56e70315337
stian.alsos@gmail.com <stian.alsos@gmail.com>
2021-01-23 00:19:56 +0100

    Added ring handler and jetty server start

--- src/cheffey/server.clj
+++ src/cheffey/server.clj
@@ -1,5 +1,16 @@
 (ns cheffey.server
-  (:gen-class))
+  (:require [reitit.ring :as ring]
+            [ring.adapter.jetty :as jetty]))
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/" {:get {:handler (fn [req] {:status 200
+                                       :body "Hello Reitit"})}}]])))
+
+(defn start []
+  (jetty/run-jetty app {:port 3000 :join? false})
+  (println "\n Server running on port 3000"))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -7,5 +18,7 @@
   (println "Hello, World!"))
 
 (comment
+  (app {:request-method :get
+        :uri "/"})
   (-main))
 
