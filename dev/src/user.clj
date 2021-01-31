a18009b9ece6f0c55a40dcabdf085d3b0a2774c9
stian.alsos@gmail.com <stian.alsos@gmail.com>
2021-01-31 19:18:36 +0100

    Added the integrant state management

---
+++ dev/src/user.clj
@@ -0,0 +1,21 @@
+(ns user
+  (:require [integrant.repl :as ig-repl]
+            [integrant.core :as ig]
+            [integrant.repl.state :as state]
+            [cheffey.server]))
+
+(ig-repl/set-prep!
+  (fn [] (-> "resources/config.edn" slurp ig/read-string)))
+
+(def go ig-repl/go)
+(def halt ig-repl/halt)
+(def reset ig-repl/reset)
+(def reset-all ig-repl/reset-all)
+
+(defn app [] (-> state/system :cheffey/app))
+(defn db [] (-> state/system :db/postgres))
+
+(comment
+  (go)
+  (halt)
+  (reset))
--- src/cheffey/server.clj
+++ src/cheffey/server.clj
@@ -1,21 +1,41 @@
 (ns cheffey.server
   (:require [reitit.ring :as ring]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty]
+            [integrant.core :as ig]
+            [environ.core :as environ]))
 
-(def app
+(defn app [env] 
   (ring/ring-handler
     (ring/router
       [["/" {:get {:handler (fn [req] {:status 200
                                        :body "Hello Reitit"})}}]])))
 
-(defn start []
-  (jetty/run-jetty app {:port 3000 :join? false})
-  (println "\n Server running on port 3000"))
+(defmethod ig/init-key :server/jetty
+  [_ {:keys [handler port]}]
+  (jetty/run-jetty handler {:port port :join? false})
+  (prn (str "\nServer running on port " port)))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defmethod ig/prep-key :server/jetty
+  [_ config]
+  (merge config {:port (Integer/parseInt (environ/env :port))}))
+
+(defmethod ig/init-key :cheffey/app
+  [_ config]
+  (prn "Started app")
+  (app config))
+
+(defmethod ig/init-key :db/postgres
+  [_ config]
+  (prn "Configured db")
+  (:jdbc-url config))
+
+(defmethod ig/halt-key! :server/jetty
+  [_ jetty]
+  (.stop jetty))
+
+(defn -main [config-file]
+  (let [config (-> config-file slurp ig/read-string)]
+    (-> config ig/prep ig/init)))
 
 (comment
   (app {:request-method :get
